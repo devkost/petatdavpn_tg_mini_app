b826c65f587be1a974b1192238c9e171ff84fbb8
devkost <konstantinyushko@gmail.com>
2026-03-10 02:43:30 +0700

    Fix frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,14 @@ import Referral from './components/Referral/Referral.jsx'
 import Payment from './components/Payment/Payment.jsx'
 import Connect from './components/Connect/Connect.jsx'
 
+window.Telegram.WebApp.expand()
+window.Telegram.WebApp.requestFullscreen()
+
 const tg = window.Telegram?.WebApp
 const tgUser = tg?.initDataUnsafe?.user
 const isTG = window.Telegram?.WebApp?.platform && 
              window.Telegram.WebApp.platform !== 'unknown'
 const startPage = isTG || tgUser ? '/home' : '/auth'
-window.Telegram.WebApp.expand()
-window.Telegram.WebApp.requestFullscreen()
 
 function App() {
     return (
