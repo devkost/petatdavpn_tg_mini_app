81e368d39f76edb359c3133179e96795dc49e0b0
devkost <konstantinyushko@gmail.com>
2026-03-09 22:19:23 +0700

    Fix frontend and add create vpn_key

--- frontend/src/components/Connect/Connect.jsx
+++ frontend/src/components/Connect/Connect.jsx
@@ -41,12 +41,6 @@ const Connect = () => {
         window.Telegram.WebApp.openLink('https://apps.apple.com/app/happ-proxy-utility/id6504287215')
     }
 
-    const handleConnect = () => {
-        if (!configLink) return
-        const deeplink = `happ://install-sub?url=${encodeURIComponent(configLink)}`
-        window.location.href = deeplink
-    }
-
     if (loading) return <div className="errorLoad">Загрузка...</div>
 
     return (
@@ -103,7 +97,6 @@ const Connect = () => {
             {/* Подключиться */}
             <button
                 className={`${styles.btnBlue} ${styles.btnFinish}`}
-                onClick={handleConnect}
                 disabled={!configLink}
             >
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
